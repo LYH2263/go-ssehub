e3a7a467257581e3598cb511e90e771613609450
LYH2263 <lyh2263@users.noreply.github.com>
2026-08-27 11:31:46 +0800

    补充验收

---
+++ snapshot.go
@@ -0,0 +1,12 @@
+package ssehub
+
+import "github.com/LYH2263/go-ssehub/internal/event"
+
+func (r *Room) SnapshotEvents() []event.Event {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	if r.Replay == nil {
+		return nil
+	}
+	return r.Replay.AliasAll()
+}
